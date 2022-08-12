2195915e90629f80d19f16bb513a55bb9a2e0f1b
AnastasiyaAD <anastasiya.dudina@mstroy.tech>
2022-08-12 13:16:08 +0200

    add BuyAdapter

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:portemonnaie/domain/model/buy/buy.dart';
 import 'package:portemonnaie/domain/model/buy/typeBuy.dart';
 import 'package:portemonnaie/domain/model/buy/shop.dart';
 import 'package:portemonnaie/domain/model/currency.dart';
@@ -13,6 +14,8 @@ void main() async {
   Directory appDocDir = await getApplicationDocumentsDirectory();
   String appDocPath = appDocDir.path;
   Hive.init(appDocPath);
+  Hive.registerAdapter(BuyAdapter());
+  await Hive.openBox<Buy>('buy');
   Hive.registerAdapter(ShopAdapter());
   await Hive.openBox<Shop>('shop');
   Hive.registerAdapter(TypeBuyAdapter());
